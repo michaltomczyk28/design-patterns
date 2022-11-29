05a0409ecb0f0f4f9ffe2b1afa659c9e47abfd0f
Michal Tomczyk <michaltomczyk28@gmail.com>
2022-11-29 23:23:22 +0100

    [SINGLETON] Implement Singleton class

---
+++ src/main/java/com/michaltomczyk/singleton/Singleton.java
@@ -0,0 +1,15 @@
+package com.michaltomczyk.singleton;
+
+public class Singleton {
+    static Singleton instance;
+
+    private Singleton(){}
+
+    public static Singleton getInstance() {
+        if(instance == null) {
+            instance = new Singleton();
+        }
+
+        return instance;
+    }
+}
